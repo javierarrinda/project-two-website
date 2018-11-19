b7dc39a584a0d90dc2761caaa73a4a526f25dfca
Javier <jarrindaulivi4@gmail.com>
2018-11-19 17:19:34 -0500

    trying to push to my blocked github

---
+++ betting-website/public/javascripts/script.js
@@ -0,0 +1,7 @@
+
+// this file is used for front-end javascript shit
+document.addEventListener('DOMContentLoaded', () => {
+
+  console.log('IronGenerator JS imported successfully!');
+
+}, false);
